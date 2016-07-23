1e18dfeedcbb13b7c3f5abfb97916a8da5dc5b81
APerson <danielhglus@gmail.com>
2016-07-23 02:19:55 -0400

    Adding basic tests

---
+++ tests/index.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3.4
+import os
+import sys
+sys.path.append('.' + '.' if 'tests/' not in __file__ else '')
+import app
+import unittest
+
+class IndexTestCase(unittest.TestCase):
+	def setUp(self):
+		self.app = app.app.test_client()
+
+	def testRoot(self):
+		response = self.app.get('/')
+		assert len(response.data) > 0
+
+if __name__ == '__main__':
+    unittest.main()
